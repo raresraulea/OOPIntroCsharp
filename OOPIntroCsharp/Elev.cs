d6bd10bdf948a72e807331b49ce0bf750d46d930
RaresRaulea <raresraulea2000@gmail.com>
2020-07-14 02:12:20 +0300

    Adaugat clasa Elev

---
+++ OOPIntroCsharp/Elev.cs
@@ -0,0 +1,30 @@
+﻿namespace OOPIntroCsharp
+{
+    public class Elev:Persoana
+    {
+        private string scoala;
+        private double mediaAnilor;
+
+        public Elev() : base(2020, "Elev Default")
+        {
+            this.scoala = "Scoala default";
+            this.mediaAnilor = 5;
+        }
+
+        public Elev(string scoala, double mediaAnilor):base(2020, "Elev SemiCustom")
+        {
+            this.scoala = scoala;
+            this.mediaAnilor = mediaAnilor;
+        }
+        public Elev(string name, int yearOfBirth, string scoala, double mediaAnilor) : base(yearOfBirth, name)
+        {
+            this.scoala = scoala;
+            this.mediaAnilor = mediaAnilor;
+        }
+
+        public override void ConsoleWrite()
+        {
+            System.Console.WriteLine(name + " - " + scoala + " - " + mediaAnilor);
+        }
+    }
+}
--- OOPIntroCsharp/Program.cs
+++ OOPIntroCsharp/Program.cs
@@ -15,6 +15,13 @@ namespace OOPIntroCsharp
             Student s2 = new Student("Toni", 1980, "Inginerie Sibiu");
             s1.ConsoleWrite();
             s2.ConsoleWrite();
+
+            Elev e1 = new Elev();
+            Elev e2 = new Elev("Elev custom", 2015, "Brukenthal", 9.8);
+            Elev e3 = new Elev("Goga", 9.5);
+            e1.ConsoleWrite();
+            e2.ConsoleWrite();
+            e3.ConsoleWrite();
         }
     }
 }
